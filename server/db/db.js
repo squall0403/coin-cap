65c8722a356f99ee19d7afc1f26382d7fa836802
Duy Nguyen <nguyenduy574@outlook.com>
2022-12-14 20:58:15 +0700

    remake server.js

--- server/db/db.js
+++ server/db/db.js
@@ -1,114 +1,30 @@
-const sqlite3 = require('sqlite3').verbose();
-const init_db = () => {
-    let db = new sqlite3.Database('./db/database.sqlite3', sqlite3.OPEN_READWRITE, (err) => {
-        if (err) {
-            console.log(err.message)
-        }
-        // console.log('Connected to the database.');
-    });
-    return (db);
-};
+var sqlite3 = require('sqlite3').verbose()
+const { resolve } = require('bluebird');
+var md5 = require('md5')
 
-const get_course = () => {
-    let db = init_db();
-    let sql = `SELECT * FROM courses;`;
+const db_source = "./db/database.sqlite3"
+
+let db = new sqlite3.Database(db_source, (err) => {
+    if (err) {
+        // Cannot open database
+        console.error(err.message)
+        throw err
+    } else {
+        console.log('Connected to the SQLite database.')
+    }
+});
+
+
+const get_all_courses = async (sql) => {
     return new Promise((resolve, reject) => {
         db.serialize(() => {
             db.all(sql, (err, rows) => {
                 if (err) {
-                    reject(err);
+                    reject(err)
                 }
-                close_db(db);
-                resolve(rows);
-            });
+                resolve(rows)
+            })
         })
     })
 }
-
-const get_single_course = (id) => {
-    let db = init_db();
-    let sql = `SELECT * FROM courses WHERE course_id=${id};`;
-    return new Promise((resolve, reject) => {
-        db.serialize(() => {
-            db.all(sql, (err, rows) => {
-                if (err) {
-                    reject(err);
-                }
-                close_db(db);
-                resolve(rows);
-            });
-        })
-    })
-}
-
-const insert_course = (courseObj) => {
-    let db = init_db();
-    let sql = `INSERT INTO courses 
-    (course_name,course_url,course_category,course_level,course_image)
-    VALUES
-    (
-        "${courseObj.course_name}",
-        "${courseObj.course_url}",
-        "${courseObj.course_category}",
-        "${courseObj.course_level}",
-        "${courseObj.course_image}"
-    )`;
-
-    return new Promise((resolve, reject) => {
-        db.serialize(() => {
-            db.run(sql, (err) => {
-                if (err) {
-                    reject(err);
-                }
-                close_db(db);
-                resolve(courseObj);
-            });
-        })
-    })
-}
-
-const delete_course = (id) => {
-    let db = init_db();
-    let sql = `DELETE from courses where course_id=${id};`
-    return new Promise((resolve, reject) => {
-        db.serialize(() => {
-            db.run(sql, (err) => {
-                if (err) {
-                    reject(err);
-                }
-                close_db(db);
-                resolve(`Course with ID: ${id} has been deleted`);
-            });
-        })
-    })
-}
-
-const close_db = (db) => {
-    db.close((err) => {
-        if (err) {
-            return console.error(err.message);
-        }
-        // console.log('Close the database connection.');
-    });
-}
-
-const create_course_db = () => {
-    db.serialize(() => {
-        db.run(`CREATE TABLE IF NOT EXISTS courses (
-            course_id INTEGER PRIMARY KEY,
-            course_name TEXT NOT NULL,
-            course_url TEXT NOT NULL,
-            course_category TEXT NOT NULL,
-            course_level TEXT NOT NULL,
-            course_image TEXT,
-        );`, (err) => {
-            if (err) {
-                console.error(err.message);
-            }
-        });
-    });
-
-    close_db();
-}
-
-module.exports = { close_db, create_course_db, get_course,get_single_course, insert_course, delete_course };
+module.exports = { get_all_courses }
--- server/server.js
+++ server/server.js
@@ -1,78 +1,22 @@
-var http = require('http');
-var url = require('url');
-const db = require('./db/db.js');
-var q;
+const express = require('express')
+const cors = require("cors");
+const app = express()
+const port = 8080
+app.use(cors());
 
-http.createServer(function (req, res) {
-  const headers = {
-    "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Methods": "OPTIONS, POST, GET",
-    "Access-Control-Max-Age": 2592000, // 30 days
-    'Content-Type': 'application/json',
-    /** add other headers as per requirement */
-  };
-  let reqUrl = req.url;
+const dbo = require("./db/db");
 
-  switch (true) {
-    case reqUrl.match('/courses') != null:
-        //get courses
-        db.get_course().then(
-          result => {
-            res.writeHead(200, headers);
-            res.write(JSON.stringify(result));
-            res.end();
-          }
-        )
-      break;
-    case reqUrl.match('/course') != null:
-      q = url.parse(req.url, true).query;
-      if (q.id != undefined) {
-        // get single course
-        db.get_single_course(q.id).then(
-          result => {
-            res.writeHead(200, headers);
-            res.write(JSON.stringify(result));
-            res.end();
-          }
-        )
-      } else {
-        res.writeHead(200, headers);
-        res.write('Missing information');
-        res.end();
-      }
-    break;
-    case reqUrl.match('/course/new') != null:
-      if (req.method === 'POST') {
-        var body = [];
-        req.on('data', (data) => {
-          body += data;
-        })
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
-        req.on('end', () => {
-          db.insert_course(JSON.parse(body)).then(
-            result => {
-              res.writeHead(200, headers);
-              res.write(JSON.stringify(result));
-              res.end();
-            }
-          );
-        });
-      }
-      break;
-    case reqUrl.match('/course/delete') != null:
-      q = url.parse(req.url, true).query;
-      db.delete_course(q.id).then(
-        result => {
-          res.writeHead(200, headers);
-          res.write(JSON.stringify(result));
-          res.end();
-        }
-      );
-      break;
-    default:
-      res.writeHead(200, headers);
-      res.end();
-      break;
-  }
+app.get("/courses", (req, res, next) => {
+  var sql = "SELECT * FROM courses"
+  dbo.get_all_courses(sql).then((results) => {
+    res.json(results)
+  });
+});
 
-}).listen(8080);
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
