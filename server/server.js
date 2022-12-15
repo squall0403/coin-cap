55d857456927d319df18304fec2cbda3985d6fd3
Duy Nguyen <duy.nguyenxuan@elca.vn>
2022-12-15 11:03:20 +0700

    add express static server

--- server/server.js
+++ server/server.js
@@ -1,13 +1,17 @@
 const express = require('express')
 const cors = require("cors");
+const serveStatic = require('serve-static')
 const app = express()
 const port = 8080
-app.use(cors());
-
 const dbo = require("./db/db");
 
+app.use(cors());
+// define storage default URL
+// to use: url to call: localhost:8080/content/[folder with content]
+app.use('/content', serveStatic('C:/Work/Sample_code/CoinCap/coin-cap/storage', { index: ['story.html',] }))
+
 app.get('/', (req, res) => {
-  res.send('')
+  res.send('/')
 })
 
 app.get("/course", (req, res, next) => {
@@ -20,7 +24,7 @@ app.get("/course", (req, res, next) => {
 app.get("/course/view/:id", (req, res, next) => {
   var sql = "SELECT * FROM courses WHERE course_id = ?"
   var params = [req.params.id]
-  dbo.get_course(sql,params).then((results) => {
+  dbo.get_course(sql, params).then((results) => {
     res.json(results)
   });
 });
@@ -28,7 +32,7 @@ app.get("/course/view/:id", (req, res, next) => {
 app.delete("/course/delete/:id", (req, res, next) => {
   var sql = "DELETE FROM courses WHERE course_id = ?"
   var params = [req.params.id]
-  dbo.delete_course(sql,params).then((results) => {
+  dbo.delete_course(sql, params).then((results) => {
     res.json(results)
   });
 });
